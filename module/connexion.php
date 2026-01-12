7b65282eee345b3a493f9b1974f6c85004c81e5d
mbouklikha <m.bouklikha@yahoo.com>
2026-01-12 11:00:53 +0100

    Organisation fichier

--- module/connexion.php
+++ module/connexion.php
@@ -12,9 +12,11 @@ class Connexion
 
     public static function initConnexion()
     {
-        self:: $bdd = new PDO("mysql:host=localhost;dbname=buvette;charset=utf8mb4", "root", "");
+        self:: $bdd = new PDO ('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201676', 'dutinfopw201676', 'hutunyzu');
 
-            // new PDO ('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201676', 'dutinfopw201676', 'hutunyzu');
+            //new PDO("mysql:host=localhost;dbname=buvette;charset=utf8mb4", "root", "");
+
+
     }
 
 
